c6be9e00aad4759004f4a581df3124ee5831c315
m4md24 <m4md24@gmail.com>
2026-08-31 05:04:12 +0300

    Adding techs to a member

--- js/data.js
+++ js/data.js
@@ -10,10 +10,12 @@ export const data = {
                 "Programming Languages": ["Java", "Kotlin", "SQL", "HTML", "CSS", "JavaScript", "C++", "C#", "Python", "PHP", "Bash"],
                 "Mobile Development": ["Jetpack Compose", "Legacy Code", "Material Design", "Room Database", "KMP"],
                 "Web Development": ["Kobweb", "Wasm"],
+                "Desktop": ["tkinter"],
                 "Backend": ["Ktor", "JWT", "RESTful APIs", "Exposed", "Firebase", "Supabase"],
+                "Other": ["Telegram Bot Development"],
                 "Databases": ["SQL", "MySQL", "SQL Server", "PostgreSQL", "HSQLDB", "SQLite", "NoSQL MongoDB", "Data Grip"],
                 "Tools & Platforms": ["Git", "GitHub", "Android Studio", "IntelliJ IDEA", "WebStorm", "Fleet", "Rider", "CLion", "PyCharm", "PhpStorm", "Docker"],
-                "Documentation": ["Markdown", "Mermaid", "JSON", "XML", "Writerside", "Libre Office", "Telegram Bot Development"],
+                "Documentation": ["Markdown", "Mermaid", "JSON", "XML", "Writerside", "Libre Office"],
                 "Design & Creativity": ["Inkscape", "Canva", "Davinci Resolve", "Logo Design", "Content Creation"]
             }
         }
